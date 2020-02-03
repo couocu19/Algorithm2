9e5c7a6b1c08a4a6c079b4a1fecf081672a8564b
coucou <1131088589@qq.com>
2020-02-04 00:58:18 +0800

    2.4

---
+++ linklist/DetectCycle.java
@@ -0,0 +1,59 @@
+package linklist;
+
+public class DetectCycle {
+
+    public static void main(String[] args) {
+        Solution142 s = new Solution142();
+        //ListNode list1 = new ListNode(2,list);
+
+        ListNode list4 = new ListNode(1);
+
+        ListNode list3 = new ListNode(2,list4);
+        //ListNode list5 = new ListNode(0, list3);
+        //list4 = new ListNode(2, list4);
+        list4 = new ListNode(1, list3);
+
+
+//        int pos = s.getIndex(list2,)
+        System.out.println(s.detectCycle(list4));
+    }
+
+
+}
+
+ class Solution142 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode p = head;
+        ListNode q = null;
+        int pos = -1;
+        int data = 0;
+        while(p!=null && p.next!=null){
+            q = p.next;
+            pos = getIndex(head,q.val);
+            System.out.println(pos);
+            if(pos<data){
+                //System.out.println(pos);
+                return q;
+            }
+            data++;
+        }
+
+        return null;
+
+    }
+
+    public int getIndex(ListNode head,int val){
+        ListNode p = head;
+        int locate = 0;
+        while(p.next!=null && p.next!=null){
+            if(p.val == val){
+                return locate;
+            }
+            p = p.next;
+            locate++;
+
+        }
+        return -1;
+
+    }
+}
