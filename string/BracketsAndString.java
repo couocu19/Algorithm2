df99feeed7ffa81bbbca709125eda82861efa267
coucou <1131088589@qq.com>
2020-04-15 09:25:42 +0800

    4.15

--- string/BracketsAndString.java
+++ string/BracketsAndString.java
@@ -1,14 +1,12 @@
 package string;
 
-import javax.xml.stream.events.Characters;
 import java.util.Collections;
 import java.util.Stack;
 
 public class BracketsAndString {
     public static void main(String[] args) {
-        String s = "3(cd1(ab))";
+        String s = "cd10(4a)";
         System.out.println(toS(s));
-
 
     }
 
@@ -19,31 +17,50 @@ public class BracketsAndString {
         Stack stack2 = new Stack();
         int t = 0;
         int f[] = new int[len];
+        int di = 0;
+        Integer num;
+        String sFlag;
+        String str;
+        String str1;
+        int lenN;
         for(int i =0;i<len;i++){
-                if(Character.isDigit(s.charAt(i)) == true){
-                    int n = Integer.parseInt(s.charAt(i)+"");
-                    stack2.push(n);
+                if(Character.isDigit(sb.charAt(i)) == true){
+                    di = i;
+                    while(Character.isDigit(sb.charAt(i)) == true){
+                        i++;
+                    }
+                    int n = Integer.parseInt(sb.substring(di,i));
+                    if(s.charAt(i) == '(') {
+                        stack2.push(n);
+                    }else{
+                        str1 = String.join("",Collections.nCopies(n,s.charAt(i)+""));
+                        sb.replace(di,i+1,str1);
+                        len = sb.length();
+                        i = i + (str1.length()-(i+2-di));
+                    }
                 }
-            if(s.charAt(i) == '('){
-                stack1.push(s.charAt(i));
-                 f[t++] = i+1;
+
+            if(sb.charAt(i) == '('){
+                stack1.push(sb.charAt(i));
+                f[t++] = i+1;
             }
-            if(s.charAt(i) == ')'){
+            if(sb.charAt(i) == ')'){
                 stack1.pop();
-                Integer num = (Integer) stack2.pop();
-                String sFlag = s.substring(f[t-1],i);
-                String str = String.join("",Collections.nCopies(num,sFlag));
-                sb.replace(f[t-1]-2,i+1,str);
-                len = len- (i+1-f[t-1]+2) + str.length();
+                num = (Integer) stack2.pop();
+                sFlag = sb.substring(f[t-1],i);
+                str = String.join("",Collections.nCopies(num,sFlag));
+                lenN = String.valueOf(num).length();
+                sb.replace(f[t-1]-1-lenN,i+1,str);
+               //每次修改完字符串都需要修改长度原字符串以及下标i
+                len = sb.length();
                 i = i + (str.length() - (i+1-f[t-1]+2));
-                s = sb.toString();
                 t--;
             }
-
         }
         return sb.toString();
 
     }
+
 }
 
 
