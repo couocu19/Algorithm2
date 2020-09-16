c605b6eb91bee9cc6ec9aa4880b9c225481e6689
coucou <1131088589@qq.com>
2020-09-16 11:54:54 +0800

    9.16

---
+++ Entry/FourNum.java
@@ -0,0 +1,69 @@
+package Entry;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourNum {
+
+    public static void main(String[] args) {
+        int[] nums = {-3,-2,-1,0,0,1,2,3};
+        Solution18s s = new Solution18s();
+
+        System.out.println(s.fourSum(nums,0));
+
+    }
+}
+
+class Solution18s {
+
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int len = nums.length;
+        int m,n;
+        int sum = 0;
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        if(len == 0)
+            return res;
+        Arrays.sort(nums);
+        for(int i =0;i<len;i++){
+//            if(i>0&& nums[i] == nums[i-1] )
+//                continue;
+            for (int j=i+1;j<len;j++){
+//                if(j>i+1 && nums[j] == nums[j-1])
+//                    continue;
+                m = j+1;
+                n = len-1;
+                while (m<n) {
+                    sum = nums[i] + nums[j] + nums[m] + nums[n];
+                    if (sum == target) {
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[m]);
+                        list.add(nums[n]);
+                        if(!res.contains(list)) {
+                            res.add(list);
+                        }
+                        list = new ArrayList<>();
+//                        while (m<n && nums[m+1] == nums[m])
+//                            m++;
+//                        while(m<n && nums[n-1] == nums[n])
+//                            n--;
+                        m++;
+                        n--;
+                    } else if (sum > target) {
+                            n--;
+                        } else{
+                            m++;
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
+
+
+
+
+}
--- Entry/FourSum.java
+++ Entry/FourSum.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class FourSum {
 
     public static void main(String[] args) {
-        int[] nums = {-3,-2,-1,0,0,1,2,3};
+        int[] nums = {-4,-3,-2,-1,0,0,1,2,3,4};
         int target = 0;
         Solution18 s = new Solution18();
         System.out.println(s.fourSum(nums,target));
@@ -20,9 +20,7 @@ class Solution18 {
     private List<Integer> list = new ArrayList<>();
     private List<List<Integer>> res = new ArrayList<>();
     private int target;
-
     private int sum;
-
     public List<List<Integer>> fourSum(int[] nums, int target) {
         this.target = target;
         int len = nums.length;
@@ -35,61 +33,44 @@ class Solution18 {
         int n = -1;
          getList(i,j,nums);
         while(i<j){
-//            if(sum == target){
-//                //i++;
-//                //j--;
-//                getList(i,j++,nums);
-//                getList(i++,j,nums);
-//                getList(i,j,nums);
-//            }else {
-            System.out.println(nums[i+1]+" "+nums[j]);
             getList(i++,j,nums);
-          //  System.out.println(res);
-
-
-            i--;
-            getList(i,j--,nums);
-
-            getList(i++,j,nums);
-           // System.out.println(i+" "+j);
-                //getList(i,j--,nums);
-//            }
         }
-//        i = 1;
-//        j = len-2;
-//        while(i<j){
-//            getList(i,j--,nums);
-//            System.out.println(j);
-//        }
+        i = 0;
+        j = len-1;
+        while(i<j){
+            getList(i,j--,nums);
+        }
+        i = 0;
+        j = len-1;
+        while (i<j){
+            getList(i++,j--,nums);
+        }
         return res;
     }
     private void getList(int i,int j,int[] nums){
-       // int sum = 0;
+        // int sum = 0;
         int m = i+1;
         int n = j-1;
-            while(m<n) {
-                if(nums[m] == nums[m-1] || nums[n] == nums[n+1]){
-                    continue;
+        while(m<n) {
+            sum = nums[i] + nums[j] + nums[m] + nums[n];
+            if (sum == target) {
+                list.add(nums[i]);
+                list.add(nums[j]);
+                list.add(nums[m]);
+                list.add(nums[n]);
+                if(!res.contains(list)) {
+                    res.add(list);
                 }
-                sum = nums[i] + nums[j] + nums[m] + nums[n];
-                if (sum == target) {
-                    System.out.println(nums[i]+" "+nums[j]+" "+nums[m]+" " +nums[n]);
-                    list.add(nums[i]);
-                    list.add(nums[j]);
-                    list.add(nums[m]);
-                    list.add(nums[n]);
-                    if(!res.contains(list)) {
-                        res.add(list);
-                    }
-                    list = new ArrayList<>();
-                    m++;
-                    n--;
-                } else if (sum > target) {
-                    n--;
-                }else{
-                    m++;
-                }
+                list = new ArrayList<>();
+                m++;
+                n--;
+            } else if (sum > target) {
+                n--;
+            }else{
+                m++;
             }
-           // return sum;
+        }
+        //return sum;
     }
+
 }
