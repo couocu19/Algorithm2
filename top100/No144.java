3d4d7aa23e5865dd947182ffc79018befd8e9fb4
couocu19 <1131088589@qq.com>
2022-10-06 20:15:55 +0800

    10.6

---
+++ top100/No144.java
@@ -0,0 +1,19 @@
+package top100;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class No144 {
+    public static void main(String[] args) {
+
+
+    }
+}
+
+class Solution144 {
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+
+    }
+}
